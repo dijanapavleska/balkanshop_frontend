f28d2f36d4414e18b8dfebb2aecfb3e4e486d4f8
Dijana <didimx.18@gmail.com>
2022-11-14 23:40:25 +0100

    images resizing

--- src/Pages/contactUs.js
+++ src/Pages/contactUs.js
@@ -1,24 +1,36 @@
 const ContactUs = () => {
   return (
     <div className="container border" style={{
-      width: '50%', backgroundImage: `url("https://img.freepik.com/free-photo/fresh-colourful-ingredients-mexican-cuisine_23-2148254294.jpg?w=1060&t=st=1668437757~exp=1668438357~hmac=bb7cdd861137cc2e1dd9266b397009499afcae443e2077a7396363860dceaecc")`,
+      backgroundImage: `url("https://img.freepik.com/free-photo/fresh-colourful-ingredients-mexican-cuisine_23-2148254294.jpg?w=1060&t=st=1668437757~exp=1668438357~hmac=bb7cdd861137cc2e1dd9266b397009499afcae443e2077a7396363860dceaecc")`,
       backgroundPosition: "center",
       backgroundSize: 'cover',
     }}
     >
+      <div className="row">
+        <div className="col-12 col-md-8 col-lg-6">
 
-      <h1 style={{ marginTop: '25px', marginLeft: '5em' }}>Contact Us</h1>
-      <form className='row' style={{ margin: '25px 85px 70px 100px', width: '60%' }}>
-        <label>Name</label>
-        <input type='text' name='name' className="form-control" />
+          <h1 style={{ marginTop: '25px', marginLeft: '1em' }}>Contact Us</h1>
+          <form className='row'>
+            <div className="col-12">
+              <label>Name</label>
+              <input type='text' name='name' className="form-control" />
+            </div>
 
-        <label>Email</label>
-        <input type='email' name='user_email' className="form-control" />
+            <div className="col-12">
 
-        <label>Message</label>
-        <textarea name="message" rows='6' style={{ marginTop: '0px', marginLeft: '12px', marginBottom: '20px' }} />
-        <input type='submit' value='send' className="form-control " />
-      </form>
+              <label>Email</label>
+              <input type='email' name='user_email' className="form-control" />
+            </div>
+
+            <div className="col-12">
+
+              <label>Message</label>
+              <textarea className="form-control" name="message" rows='6' />
+              <input type='submit' value='send' className="form-control" style={{ marginTop: '1em' }} />
+            </div>
+          </form>
+        </div>
+      </div>
     </div >
   )
 }
