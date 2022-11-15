57043c6aa6a8096367af729e95a06b7c40979b3d
Dijana <didimx.18@gmail.com>
2022-11-15 16:06:44 +0100

    images

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
+import React from "react";
+import { BrowserRouter, Routes, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ContactUs from "./Pages/ContactUs";
+import AboutUs from "./Pages/AboutUs";
 import Home from "./Pages/Home.jsx";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <Home />
-      <div>
+    <>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about-us' element={<AboutUs />} />
+          <Route path='/contact-us' element={<ContactUs />} />
+        </Routes>
         <Footer />
-        <ContactUs />
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Footer.css';
-
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div className='container'>
+        <div className='footer'>
             <div className='row'>
                 {/* Column1 */}
                 <div className="col-12 col-md-3">
@@ -13,9 +13,9 @@ const Footer = () => {
                 <div className='col-12 col-md-3'>
                     <h4>Company</h4>
                     <ul className='footer-box ps-2'>
-                        <li><a href="#">About Us</a></li>
+                        <li><Link to="/about-us">About Us</Link></li>
                         <li><a href="#">General Terms and Conditions</a></li>
-                        <li><a href="#">Contact Form</a></li>
+                        <li><Link to="/contact-us">Contact Form</Link></li>
                     </ul>
                 </div>
                 {/* COlumn2 */}
